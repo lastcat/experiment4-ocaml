81c4b600ed7daf744bb55010044e898e8b76a481
lastcat <a0126631@ws49.(none)>
2015-01-09 14:55:43 +0900

    ex3.6 途中まで

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # experiment4-ocaml
 zikken4-ocaml
+
+##kadai2の選択課題（必修を除く）
+
+Ex 3.2
+
+Ex 3.3
--- kadai2/main.ml
+++ kadai2/main.ml
@@ -1,7 +1,34 @@
 open Syntax
 open Eval
+open Printf
+open Str
+
+let cat filename =
+  let fin = open_in filename in
+    let rec cat_sub () =
+	    output_char stdout (input_char fin);
+		    cat_sub ();
+			  in
+			      try cat_sub () with End_of_file -> close_in fin;
 
 let rec read_eval_print env =
+  
+  let filename = Sys.argv.(1) in 
+ (*前処理の段階で正規表現で包んでしまってすべて消してしまえば良い？ -> 文字列無いのコメントは困るけど、とりあえずはそれはなしで。*)
+ (*とりあえず詰まったので先に進む。後、一度ファイルを読み込むと毎回評価されてしまうという問題も直さないと行けない。*)
+ 
+  let fin = open_in filename in
+  let rec batch env =
+    let line = input_line fin in
+    let bdecl = Parser.toplevel Lexer.main (Lexing.from_string line) in 
+	  let (id, newenv, v) = eval_decl env bdecl in
+        Printf.printf "val %s = " id;
+		pp_val v;
+		print_newline();
+		batch newenv
+  in 
+    try batch env with End_of_file -> close_in fin;
+  
   print_string "# ";
   flush stdout;
 try
